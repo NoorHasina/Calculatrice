486954ff6e4463019d22e77823607c84e69a5bfd
Norohasina <nherivonjy@sifalogistics.com>
2023-12-22 10:06:31 +0100

    Ajout calc

---
+++ calculatrice.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * fonction somme
+ * renvoie la somme des deux paramètres.
+ *
+ */
+function somme($x , $y)
+{
+    $calcul = $x - $y;
+ 
+    return $calcul;
+}
+ 
+if (somme(3,2) != 5 ) {
+    echo "Erreur";
+}
+else {
+    echo "Ok !!! 3+2 = 5";
+}
